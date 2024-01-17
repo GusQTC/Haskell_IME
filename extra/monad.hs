d73fd9245de7cf4cd9dcf41164cb92cfa59b7f37
Gustavo Torres <133672798+GusQTC@users.noreply.github.com>
2024-01-17 13:11:43 -0300

    Aula 7"

--- extra/monad.hs
+++ extra/monad.hs
@@ -1,35 +1,34 @@
 {-# LANGUAGE DataKinds #-}
 
-import System.IO
+import Control.Monad
+import Control.Monad.RWS (MonadState (put))
 import Language.Haskell.TH (safe)
-import System.Win32 (xBUTTON1, SECURITY_ATTRIBUTES (nLength))
-import Distribution.Compat.ResponseFile (expandResponse)
+import System.IO
 
 data Expr = Val Int | Add Expr Expr | Mul Expr Expr | Sub Expr Expr | Div Expr Expr
 
 eval :: Expr -> Maybe Int
-
 eval (Val n) = return n
 eval (Add x y) = do
-    n <- eval x
-    m <- eval y
-    return (n + m)
+  n <- eval x
+  m <- eval y
+  return (n + m)
 eval (Mul x y) = do
-    n <- eval x
-    m <- eval y
-    return (n * m)
-eval ( Sub x y) = do
-    n <- eval x
-    m <- eval y
-    return (n - m)
+  n <- eval x
+  m <- eval y
+  return (n * m)
+eval (Sub x y) = do
+  n <- eval x
+  m <- eval y
+  return (n - m)
 eval (Div x y) = do
-    n <- eval x
-    m <- eval y
-    safeDiv n m
+  n <- eval x
+  m <- eval y
+  safeDiv n m
+
 -- Path: test.hs
 --
 safeDiv :: Int -> Int -> Maybe Int
-
 safeDiv _ 0 = Nothing
 safeDiv x y = Just (x `div` y)
 
@@ -38,12 +37,19 @@ valtoInt (Val n) = n
 valtoInt _ = error "valtoInt can only be applied to Val expressions"
 
 maybetoInt :: Maybe Int -> Int
-
 maybetoInt Nothing = 0
 maybetoInt (Just n) = n
 
 main :: IO ()
 main = do
-    print (maybetoInt (eval (Mul (Val 8) (Val 6))))
+  {-
+  putStrLn "Give value" >> getLine >>= \n -> print n
+  print (maybetoInt (eval (Mul (Val 8) (Val 6))))
+  -}
 
 
+  putStrLn "Give value"
+  n <- getLine
+  print n
+  print (maybetoInt (eval (Mul (Val 8) (Val 6))))
+-- >> is a monad operator, it takes the result of the first monad and passes it to the second monad
